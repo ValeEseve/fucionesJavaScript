959d75379fd4ef51560eea11239a8b1c21288243
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-04-13 19:48:33 -0400

    Correccion ejercicio 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var elemento = document.getElementById('ele1')
 elemento.style.backgroundColor = "green"
 
 function pintar(ele, color){
-    let ele.style.backgroundColor = color
+    ele.style.backgroundColor = color
     }
 
 elemento.addEventListener("click", function(ele){
-    color = "yellow"
+    let color = "yellow"
     pintar(ele.target, color)
 })
